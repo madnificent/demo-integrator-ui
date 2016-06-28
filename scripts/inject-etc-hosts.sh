442fecc8ee4e57443ec5cc57b5a6dee34cdaacf8
Aad Versteden <madnificent@gmail.com>
2016-06-28 15:29:27 +0200

    Update host injection script
    We have a constrained world with different hosts.  Updating.

--- scripts/inject-etc-hosts.sh
+++ scripts/inject-etc-hosts.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-HOSTS="hadoop.sc1.big-data-europe.eu hdfsfb.sc1.big-data-europe.eu spark.sc1.big-data-europe.eu solr.sc1.big-data-europe.eu flink.sc1.big-data-europe.eu strabon.sc1.big-data-europe.eu sextant.sc1.big-data-europe.eu frontend.sc1.big-data-europe.eu"
+echo "# demo-spark-sensor-data-integrator-ui" | sudo tee -a /etc/hosts
+echo "127.0.0.1  hdfs-namenode.demo.big-data-europe.local  hdfs-resourcemanager.demo.big-data-europe.local hdfs-nodemanager.demo.big-data-europe.local hue.demo.big-data-europe.local spark-master.demo.big-data-europe.local spark-worker.demo.big-data-europe.local demo.big-data-europe.local monitor.demo.big-data-europe.local virtuoso.demo.big-data-europe.local" | sudo tee -a /etc/hosts;
 
-for host in $HOSTS; do
-  echo "127.0.0.1 $host" | sudo tee -a /etc/hosts; 
-done
 
